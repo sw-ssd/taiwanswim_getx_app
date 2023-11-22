42b274264b6d3b48114312652230be7391087b6c
sowiner <sowiner@gmail.com>
2023-11-22 13:42:41 +0800

    fixed

--- lib/app/data/providers/signin_provider.dart
+++ lib/app/data/providers/signin_provider.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pocketbase/pocketbase.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import 'package:taiwanswim_getx_app/app/data/constants/api_paths.dart';
 import 'package:taiwanswim_getx_app/utils/tools.dart';
@@ -15,8 +14,7 @@ const String collectName = "members";
 const String appleRedirectUri = "members";
 
 class SigninProvider extends GetConnect {
-  final String googleRedirectUri = "http://127.0.0.1:8090/api/oauth2-redirect";
-  // final String googleRedirectUri = "${getEnvBaseUrl()}/api/oauth2-redirect";
+  final String googleRedirectUri = "${getEnvBaseUrl()}/api/oauth2-redirect";
   final clinet = GetIt.I.get<PocketBase>();
   RxString googleId = "".obs;
   @override
@@ -35,31 +33,26 @@ class SigninProvider extends GetConnect {
           .where((am) => am.name.toLowerCase() == googleOAuthName)
           .first;
 
-      final authData = await client.collection('users').authWithOAuth2('google',
-          (url) async {
-        await launchUrl(url);
-      });
+      print(google);
 
-      print(authData);
+      final responseUrl = await FlutterWebAuth.authenticate(
+          url: "${google.authUrl}$googleRedirectUri",
+          callbackUrlScheme: "http");
 
-      // final responseUrl = await FlutterWebAuth2.authenticate(
-      //     url: "${google.authUrl}$googleRedirectUri",
-      //     callbackUrlScheme: "https");
+      print(responseUrl);
 
-      // print(responseUrl);
+      final parsedUri = Uri.parse(responseUrl);
+      final code = parsedUri.queryParameters['code']!;
 
-      // final parsedUri = Uri.parse(responseUrl);
-      // final code = parsedUri.queryParameters['code']!;
+      final state = parsedUri.queryParameters['state']!;
+      if (google.state != state) {
+        throw "oops";
+      }
 
-      // final state = parsedUri.queryParameters['state']!;
-      // if (google.state != state) {
-      //   throw "oops";
-      // }
+      var result = await client.collection(collectName).authWithOAuth2Code(
+          googleOAuthName, code, google.codeVerifier, googleRedirectUri);
 
-      // var result = await client.collection(collectName).authWithOAuth2Code(
-      //     googleOAuthName, code, google.codeVerifier, googleRedirectUri);
-
-      // googleId.value = result.token;
+      googleId.value = result.token;
     } catch (e) {
       debugPrint(e.toString());
       rethrow;
