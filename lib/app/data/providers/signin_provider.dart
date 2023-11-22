725aa6b155f4e6958e9ac267713475be68cc08d2
sowiner <sowiner@gmail.com>
2023-11-22 17:37:48 +0800

    recheck

--- lib/app/data/providers/signin_provider.dart
+++ lib/app/data/providers/signin_provider.dart
@@ -4,6 +4,7 @@ import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pocketbase/pocketbase.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:taiwanswim_getx_app/app/data/constants/api_paths.dart';
 import 'package:taiwanswim_getx_app/utils/tools.dart';
@@ -37,7 +38,7 @@ class SigninProvider extends GetConnect {
 
       final responseUrl = await FlutterWebAuth.authenticate(
           url: "${google.authUrl}$googleRedirectUri",
-          callbackUrlScheme: "http");
+          callbackUrlScheme: "https");
 
       print(responseUrl);
 
@@ -53,6 +54,14 @@ class SigninProvider extends GetConnect {
           googleOAuthName, code, google.codeVerifier, googleRedirectUri);
 
       googleId.value = result.token;
+
+      // final authData = await client
+      //     .collection(collectName)
+      //     .authWithOAuth2(googleOAuthName, (url) async {
+      //   await launchUrl(url);
+      // });
+
+      // print(authData);
     } catch (e) {
       debugPrint(e.toString());
       rethrow;
